e44b2e2f8144543e118e3e6a23966961f3174b61
Charul00 <charulchim18@gmail.com>
2024-10-14 23:42:23 +0530

    added_id_response

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -19,52 +19,71 @@ const signup = async (req, res) => {
     try {
       const { name, email, password } = req.body;
       const user = await UserModel.findOne({ email });
+      
       if (user) {
         return res.status(409).json({
           message: "User already exists, you can login",
           success: false,
         });
       }
+      
       const newUser = new UserModel({ name, email, password });
       newUser.password = await bcrypt.hash(password, 10);
       await newUser.save();
-      res.status(201).json({ message: "Signup successfully", success: true });
+
+      // Return the _id along with the signup success message
+      res.status(201).json({
+        message: "Signup successfully",
+        success: true,
+        _id: newUser._id,   // Include the MongoDB _id in the response
+        name: newUser.name,
+        email: newUser.email
+      });
+      
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Internal server error", success: false });
     }
-  };
+};
+
 
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await UserModel.findOne({ email });
         const errorMsg = 'Auth failed. Email or password is wrong';
+        
         if (!user) {
             return res.status(403).json({ message: errorMsg, success: false });
         }
+        
         const isPassEqual = await bcrypt.compare(password, user.password);
+        
         if (!isPassEqual) {
             return res.status(403).json({ message: errorMsg, success: false });
         }
+        
         const jwtToken = jwt.sign(
             { email: user.email, _id: user._id },
             process.env.JWT_SECRET,
             { expiresIn: '24h' }
         );
 
+        // Return the _id along with the other fields
         res.status(200).json({
             message: "Login success",
             success: true,
             jwtToken,
             email,
-            name: user.name
+            name: user.name,
+            _id: user._id   // Include the MongoDB _id in the response
         });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Internal server error", success: false });
     }
 };
+
 
 
 
